0e8e91c551360a3efc0edad99cb8793c0750d1ac
Fidellis-Taz <fidelishoca@gmail.com>
2020-11-21 23:53:26 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>eLearn-Africa</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+
+<link rel="stylesheet" href="css/style.css">
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+<!-- start Nav -->
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
+  <a class="navbar-brand" href="index.php">eLearn</a>
+  <span class="navbar-text">Learn wthout Hustle</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <ul class="navbar-nav custom-nav pl-5" >
+       <li class="nav-item custom-nav-item" ><a href="index.php" class="nav-link" >Home</a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >Courses</a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >Payment Status</a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >My Profile</a></li>
+       <li class="nav-item custom-nav-item" ><a href="" class="nav-link" >Logout</a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >Login</a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >SignUp </a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >FeedBack </a></li>
+       <li class="nav-item custom-nav-item" ><a href="#" class="nav-link" >Contact </a></li>
+    </ul>
+  </div> 
+</nav>
+
+<!-- end Nav -->
+
+<!-- Start Video Backgroun -->
+
+<div class="container-fluid remove-vid-marg">
+    <div class="vid-parent">
+        <video playsinline autoplay muted loop >
+            <source src="video/bak.mp4">
+        </video>
+        <div class="vid-overlay"></div>
+    </div>
+    <div class="vid-content">
+        <h1 class="my-content">Welcome To eLearn</h1>
+        <small class="my-content">Learn and İmplement</small>
+        <a href="#" class="btn btn-danger">Get Started</a>
+    </div>
+</div>
+<!-- end Video Backgroun --> 
+
+
+
+<!-- start Text Banner -->
+<div class="container-fluid bg-danger txt-banner">
+
+<div class="row bottom-banner">
+<div class="col-sm">
+    <h5><i class="fas fa-book-open mr-3"></i>100+ Online Courses</h5>
+</div>
+<div class="col-sm">
+    <h5><i class="fas fa-users mr-3"> </i>Expert İnstructors</h5>
+</div>
+<div class="col-sm">
+    <h5><i class="fas fa-keyboard mr-3"> </i>Lifetime Access</h5>
+</div>
+<div class="col-sm">
+    <h5><i class="fas fa-rupee-sign mr-3"> </i>Money Back Guarantee</h5>
+</div>
+</div>
+
+</div>
+<!-- end Text Banner -->
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
+</body>
+</html>
